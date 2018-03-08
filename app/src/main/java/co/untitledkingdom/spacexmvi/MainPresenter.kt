9b87da7539a2c3d38ba48b71f07157ff7e4f8c9c
Mateusz Dziubek <matdziu@gmail.com>
2018-03-08 13:15:57 +0100

    Now changing UI events to partial states

--- app/src/main/java/co/untitledkingdom/spacexmvi/MainPresenter.kt
+++ app/src/main/java/co/untitledkingdom/spacexmvi/MainPresenter.kt
@@ -2,22 +2,8 @@ package co.untitledkingdom.spacexmvi
 
 class MainPresenter(private val mainInteractor: MainInteractor) {
 
-    private lateinit var mainView: MainView
-
     fun bind(mainView: MainView) {
-        this.mainView = mainView
-    }
-
-    fun getRocketList() {
-        mainView.showProgressBar(true)
-        mainInteractor.fetchRocketList().subscribe(
-                {
-                    mainView.showProgressBar(false)
-                    mainView.showRocketList(it)
-                },
-                {
-                    mainView.showProgressBar(false)
-                    mainView.showError(true)
-                })
+        val buttonClickObservable = mainView.emitButtonClick()
+                .flatMap { mainInteractor.fetchRocketList() }
     }
 }
