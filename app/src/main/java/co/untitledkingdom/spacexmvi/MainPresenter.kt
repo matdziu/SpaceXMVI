aa6a96d7d8bb6079590b67da2219889999874fa0
Mateusz Dziubek <matdziu@gmail.com>
2018-03-08 15:01:22 +0100

    Added unbind method

--- app/src/main/java/co/untitledkingdom/spacexmvi/MainPresenter.kt
+++ app/src/main/java/co/untitledkingdom/spacexmvi/MainPresenter.kt
@@ -1,15 +1,24 @@
 package co.untitledkingdom.spacexmvi
 
 import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
 
 class MainPresenter(private val mainInteractor: MainInteractor) {
+
+    private val compositeDisposable = CompositeDisposable()
 
     fun bind(mainView: MainView) {
         val buttonClickObservable = mainView.emitButtonClick()
                 .flatMap { mainInteractor.fetchRocketList().startWith(PartialMainViewState.ProgressState()) }
 
         val mergedIntentsObservable = Observable.merge(listOf(buttonClickObservable))
-        mergedIntentsObservable.scan(MainViewState(), this::reduce).subscribe { mainView.render(it) }
+        compositeDisposable.add(
+                mergedIntentsObservable.scan(MainViewState(), this::reduce).subscribe { mainView.render(it) }
+        )
+    }
+
+    fun unbind() {
+        compositeDisposable.clear()
     }
 
     private fun reduce(previousState: MainViewState, partialState: PartialMainViewState): MainViewState {
