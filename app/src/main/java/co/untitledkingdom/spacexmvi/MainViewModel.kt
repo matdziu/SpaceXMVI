204801939e0000842b286cdd0f8e412be62d05cb
Mateusz Dziubek <matdziu@gmail.com>
2018-04-06 10:08:16 +0200

    Now tracking full state

--- app/src/main/java/co/untitledkingdom/spacexmvi/MainViewModel.kt
+++ app/src/main/java/co/untitledkingdom/spacexmvi/MainViewModel.kt
@@ -8,16 +8,16 @@ import io.reactivex.subjects.BehaviorSubject
 class MainViewModel(private val mainInteractor: MainInteractor = MainInteractor()) : ViewModel() {
 
     private val compositeDisposable = CompositeDisposable()
-    private val stateSubject = BehaviorSubject.create<PartialMainViewState>()
+    private val stateSubject = BehaviorSubject.createDefault(MainViewState())
 
     fun bind(mainView: MainView) {
         val buttonClickObservable = mainView.emitButtonClick()
                 .flatMap { mainInteractor.fetchRocketList().startWith(PartialMainViewState.ProgressState()) }
 
-        val mergedIntentsObservable = Observable.merge(listOf(buttonClickObservable)).subscribeWith(stateSubject)
-        compositeDisposable.add(
-                mergedIntentsObservable.scan(MainViewState(), this::reduce).subscribe { mainView.render(it) }
-        )
+        val mergedIntentsObservable = Observable.merge(listOf(buttonClickObservable))
+        mergedIntentsObservable.scan(stateSubject.value, this::reduce).subscribeWith(stateSubject)
+
+        compositeDisposable.add(stateSubject.subscribe { mainView.render(it) })
     }
 
     fun unbind() {
